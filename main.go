3074714b2f5a392b0782a1a2bfcb3af69826bd14
hola <franco.giannotti@mercadolibre.com>
2022-02-05 12:24:18 -0300

    add logger and gin router

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+
+	defer logger.Sync()
+
+	sugar := logger.Sugar()
+	sugar.Infow("Running go server...")
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) { c.JSON(200, gin.H{"message": "pong"}) })
+	r.Run(":8080")
 }
